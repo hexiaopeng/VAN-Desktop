bc63cca753bd4f4debc0bfb30390f4f45132cbd0
hexiaopeng <252949842@qq.com>
2017-03-20 14:40:51 +0800

    change 20

--- js/home.js
+++ js/home.js
@@ -1,8 +1,7 @@
 function BookHome(){
-    this.search = window.location.search.substring(1);
     this.hash = window.location.hash.substring(1);
-    this.source = $.getUrl(this.search,'t') || 'local';
-    this.isOpen = $.getUrl(this.hash,'o') == 1 ? true : false;
+    this.source = $.getUrl(this.hash,'t') || 'local';
+    this.isOpen = false;
 }
 
 BookHome.prototype = {
@@ -19,14 +18,37 @@ BookHome.prototype = {
 
         };
         this.addTypeList();
-        // $.getEle('.bg').onclick = function(ev){
-        //     _this.ripple(this,ev)
-        // }
-        console.log($.getEle('.online'));
-        $.getEle('.online').addEventListener('click',function(){
+        $.getEle('.online').addEventListener('click',function(ev){
+            _this.ripple(this,ev);
             _this.openList(this);
-        })
+        });
+        $.getEle('.lately').addEventListener('click',function(ev){
+            _this.ripple(this,ev);
+        });
+        $.getEle('.local').addEventListener('click',function(ev){
+            _this.ripple(this,ev);
+            location.hash = 't=local&page=1';
+        });
+        $.getEle('.search').addEventListener('click',function(ev){
+            location.hash = 't=search&keyword=' + $.getEle.call($.getEle('.nav_search'),'input').value;
+        });
 
+        window.onhashchange = function(){
+            _this.source = $.getUrl(location.hash,'t') || 'local';
+            _this.hash = window.location.hash.substring(1);
+            if (_this.source == 'search') {
+                $.getEle('#BookLoad').style.display = 'block';
+                _this.searchBook();
+            }else if (_this.source == 'online') {
+                $.getEle('#BookLoad').style.display = 'block';
+                _this.online();
+            }else if (_this.source == 'local') {
+                $.getEle('#BookLoad').style.display = 'block';
+                _this.local();
+            }else {
+
+            };
+        }
     },
     local:function(){
         var _this = this;
@@ -35,6 +57,7 @@ BookHome.prototype = {
         var num = null;
         if (this.con == 'list') {
             page = Number($.getUrl(this.hash,'page')) || 1;
+            $.getEle('#BookLoad').style.display = 'none';
             this.data = {
                 data:data.getFileByClass(data.nodes, 'book').slice((page-1)*20,page*20)
             };
@@ -45,6 +68,7 @@ BookHome.prototype = {
             $.ajax({
                 url:'book/' + obj.title + '.txt',
                 success:function(data){
+                    $.getEle('#BookLoad').style.display = 'none';
                     _this.data = {
                         name:obj.name,
                         author:obj.author,
@@ -70,6 +94,7 @@ BookHome.prototype = {
                 url:'http://route.showapi.com/211-2',
                 data:'showapi_appid=33871&showapi_sign=1f10779654da46eaa4316bd4df768e93&keyword='+ keyword + '&page=' + page,
                 success:function(data){
+                    $.getEle('#BookLoad').style.display = 'none';
                     _this.data = {
                         data:JSON.parse(data).showapi_res_body.pagebean.contentlist
                     }
@@ -82,6 +107,7 @@ BookHome.prototype = {
                 url:'http://route.showapi.com/211-1',
                 data:'showapi_appid=33871&showapi_sign=1f10779654da46eaa4316bd4df768e93&bookId='+ num,
                 success:function(data){
+                    $.getEle('#BookLoad').style.display = 'none';
                     var obj = JSON.parse(data);
                     if (obj.showapi_res_body.ret_code != 0) {
                         $.getEle('.BookList').innerHTML = '没有找到您要的小说';
@@ -102,6 +128,7 @@ BookHome.prototype = {
     online:function(){
         var _this = this;
         this.con = $.getUrl(this.hash,'c') || 'list';
+        console.log(this.hash);
         var typeId = null;
         var num = null;
         if (this.con == 'list') {
@@ -111,6 +138,7 @@ BookHome.prototype = {
                 url:'http://route.showapi.com/211-2',
                 data:'showapi_appid=33871&showapi_sign=1f10779654da46eaa4316bd4df768e93&typeId='+ typeId + '&page=' + page,
                 success:function(data){
+                    $.getEle('#BookLoad').style.display = 'none';
                     _this.data = {
                         data:JSON.parse(data).showapi_res_body.pagebean.contentlist
                     }
@@ -123,6 +151,7 @@ BookHome.prototype = {
                 url:'http://route.showapi.com/211-1',
                 data:'showapi_appid=33871&showapi_sign=1f10779654da46eaa4316bd4df768e93&bookId='+ num,
                 success:function(data){
+                    $.getEle('#BookLoad').style.display = 'none';
                     var obj = JSON.parse(data);
                     if (obj.showapi_res_body.ret_code != 0) {
                         $.getEle('.BookList').innerHTML = '没有找到您要的小说';
@@ -149,8 +178,8 @@ BookHome.prototype = {
                 var arr = JSON.parse(data).showapi_res_body.typeList;
                 var html = '';
                 for (var i = 0; i < arr.length; i++) {
-                    html += '<li>'
-                    +           '<a data-type="'+arr[i].id+'" href="javascript:;">'
+                    html += '<li data-type="'+arr[i].id+'">'
+                    +           '<a href="javascript:;">'
                     +               arr[i].name
                     +               '<span class="ink"></span>'
                     +           '</a>'
@@ -158,11 +187,17 @@ BookHome.prototype = {
                 }
                 $.getEle('.search_list').innerHTML = html;
                 _this.lists = $.getEle('.search_list li');
+                for (var i = 0; i < _this.lists.length; i++) {
+                    _this.lists[i].addEventListener('click',function(ev){
+                        location.hash = 't=online&typeId=' + this.dataset.type;
+                        event.stopPropagation();
+                    });
+                }
             }
         })
     },
     ripple:function(That,ev){
-        var span = $.getEle.call(That,'span');
+        var span = $.getEle.call(That,'span')[0] || $.getEle.call(That,'span');
         if(span){
             span.className = 'ink';
             span.style.width = That.offsetWidth + 'px';
@@ -176,14 +211,14 @@ BookHome.prototype = {
         var ul = $.getEle.call(That,'ul');
         var icon = $.getEle.call(That,'.more');
         if (ul) {
-            if (this.onOff) {
+            if (this.isOpen) {
                 $.move(ul, {height: 0}, 300, 'easeOut');
                 $.removeClass(icon,'moreClick');
-                this.onOff = false;
+                this.isOpen = false;
             }else{
                 $.move(ul, {height: 48 * this.lists.length}, 300, 'easeOut');
                 $.addClass(icon,'moreClick');
-                this.onOff = true;
+                this.isOpen = true;
             }
         }
     }
